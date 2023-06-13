8cf0a0da67729888bc73392905ecee7dce281da8
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-06-12 19:00:39 -0700

    Add Todo I/O function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  tasks = [
+    { title: 'workout', done: false },
+    { title: 'job hunting', done: true },
+    { title: 'studying English', done: false },
+  ];
+  newTaskTitle = '';
+  addTask() {
+    this.tasks.push({ title: this.newTaskTitle, done: false });
+    this.newTaskTitle = '';
+  }
 }
